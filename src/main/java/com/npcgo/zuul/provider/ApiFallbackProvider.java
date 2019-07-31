9651b382945f4ac2fe39604e4f86c931d659b0d7
snow <long_2005_08@126.com>
2019-07-31 16:38:58 +0800

    config

--- src/main/java/com/npcgo/zuul/provider/ApiFallbackProvider.java
+++ src/main/java/com/npcgo/zuul/provider/ApiFallbackProvider.java
@@ -18,11 +18,13 @@ import java.io.InputStream;
 public class ApiFallbackProvider implements FallbackProvider {
     @Override
     public String getRoute() {
-        return "spring-snow-eureka-client";//该Provider应用的Route ID，例如：spring-snow-eureka-client，如果设置为 * ，那就对所有路由生效
+//        return "spring-snow-eureka-client";//该Provider应用的Route ID，例如：spring-snow-eureka-client，如果设置为 * ，那就对所有路由生效
+        return "*";//该Provider应用的Route ID，例如：spring-snow-eureka-client，如果设置为 * ，那就对所有路由生效
+
     }
 
     //快速回退失败/响应，即处理异常并返回对应输出/响应内容。route：发生异常的RouteID，cause：触发快速回退/失败的异常/错误
-    @Override
+   @Override
     public ClientHttpResponse fallbackResponse(String route, Throwable cause) {
         log.warn(String.format("route:%s,exceptionType:%s,stackTrace:%s", route, cause.getClass().getName(), cause.getStackTrace()));
 
@@ -36,6 +38,7 @@ public class ApiFallbackProvider implements FallbackProvider {
     }
 
     //Spring提供的HttpResponse接口。可以通过实现该接口自定义Http status、body、header
+
     public ClientHttpResponse fallbackResponse(String message) {
 
         return new ClientHttpResponse() {
